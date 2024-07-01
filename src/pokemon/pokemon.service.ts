f622ba34f291be6644176cdd34252262f2aeef78
Daniel Felipe Rosero Zemanate <daniel.zemanate@correounivalle.edu.co>
2024-07-01 09:51:50 -0500

    Adding stuff to deployment

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,13 +5,20 @@ import { Pokemon } from './entities/pokemon.entity';
 import { Model, isValidObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDTO } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService
+  ) {
+    this.defaultLimit = configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -26,7 +33,7 @@ export class PokemonService {
   }
 
   findAll(paginationDTO: PaginationDTO) {
-    const {limit = 10, offset = 0} = paginationDTO;
+    const {limit = this.defaultLimit, offset = 0} = paginationDTO;
 
     return this.pokemonModel.find()
     .limit(limit)
